1ec3ce63b791b2d2ae24346c650419fba201f23b
Majid Amiri <majidamiriii@gmail.com>
2021-01-20 21:42:22 +0330

    improve cover image display

--- components/IndexPage/Content.js
+++ components/IndexPage/Content.js
@@ -4,12 +4,13 @@ import {
   ChipBadge,
   TinyBadge,
   DetailsTable,
+  Flex,
   H1,
   H3,
   Text,
   Icon,
   Image,
-  Paragraph,
+  ImageModal,
   HistoryTree,
 } from "@licenserocks/kit";
 import styled from "styled-components";
@@ -32,29 +33,17 @@ const HeaderContainer = styled.div`
   }
 `;
 
-const StyledDetailsTable = styled(DetailsTable)``;
-
-const AbstractContentContainer = styled.div`
-  display: flex;
-  margin-right: 32px;
-  padding: 24px 0;
-  border-bottom: 1px solid ${({ theme }) => theme.palette.secondary.light};
-`;
-
 const StyledImage = styled(Image)`
-  max-width: 200px;
-  margin-right: 24px;
-  box-sizing: border-box;
-`;
+  width: 50%;
+  height: auto;
+  max-height: 250px;
+  object-fit: fill;
+  border-radius: 16px;
+  margin-top: ${({ theme }) => theme.spacing(4)};
 
-const MainContentContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 220px;
-  margin-right: 32px;
-`;
-
-const HistoryTreeContainer = styled.div`
-  margin-top: 40px;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    width: 100%;
+  }
 `;
 
 const CryptoIcon = styled(Icon).attrs({
@@ -170,7 +159,6 @@ export const IndexContent = withTranslation("index")(
     network,
     histories,
     coverSrc,
-    abstract,
     t,
     fileURI: _fileURI,
     _documents,
@@ -185,6 +173,7 @@ export const IndexContent = withTranslation("index")(
     const [activeHistory, setActiveHistory] = useState(orderedHistories[0]);
     const [txInfo, setTxInfo] = useState(null);
     const [txLoading, setTxLoading] = useState(false);
+    const [coverPreviewOpen, setCoverPreviewOpen] = useState(false);
 
     const getTransactionInfo = () => {
       setTxLoading(true);
@@ -199,40 +188,50 @@ export const IndexContent = withTranslation("index")(
     }, []);
 
     return (
-      <>
-        <HeaderContainer>
-          <H1 content={title} />
-          {exclusive && <TinyBadge label={t("exclusive")} color="warning" />}
-          {unique && <TinyBadge label={t("unique")} color="success" />}
-        </HeaderContainer>
-        <Text color="textSecondary" mb={2}>
-          {t("network")}:
-          <Text
-            color="textPrimary"
-            content={` ${network}`}
-            dInline
-            fontWeight="bold"
-          />
-        </Text>
+      <Flex container alignItems="flex-start">
+        <Flex item xs={12} md={8}>
+          <HeaderContainer>
+            <H1 content={title} />
+            {exclusive && <TinyBadge label={t("exclusive")} color="warning" />}
+            {unique && <TinyBadge label={t("unique")} color="success" />}
+          </HeaderContainer>
+          <Text color="textSecondary" mb={2}>
+            {t("network")}:
+            <Text
+              color="textPrimary"
+              content={` ${network}`}
+              dInline
+              fontWeight="bold"
+            />
+          </Text>
 
-        {childId === "0" && (
-          <ChipBadge color="success" label="Updated" icon="check-circle" />
-        )}
+          {childId === "0" && (
+            <ChipBadge color="success" label="Updated" icon="check-circle" />
+          )}
 
-        {childId !== "0" && (
-          <Row>
-            <ChipBadge color="error" label="Outdated" />
-            <Link href={getChildLink(childId)}>See latest version</Link>
-          </Row>
-        )}
+          {childId !== "0" && (
+            <Row>
+              <ChipBadge color="error" label="Outdated" />
+              <Link href={getChildLink(childId)}>See latest version</Link>
+            </Row>
+          )}
 
-        <AbstractContentContainer>
-          <StyledImage src={coverSrc} />
-          <Paragraph content={abstract} />
-        </AbstractContentContainer>
+          {coverSrc && (
+            <>
+              <StyledImage
+                src={coverSrc}
+                onClick={() => setCoverPreviewOpen(true)}
+              />
 
-        <MainContentContainer>
-          <StyledDetailsTable
+              <ImageModal
+                imgSrc={coverSrc}
+                isOpen={coverPreviewOpen}
+                onClose={() => setCoverPreviewOpen(false)}
+              />
+            </>
+          )}
+
+          <DetailsTable
             labelTextTransform="capitalize"
             my={10}
             labelWidth={220}
@@ -273,44 +272,45 @@ export const IndexContent = withTranslation("index")(
                   : []
               )}
           />
-          <HistoryTreeContainer>
-            <HistoryPicker
-              historyItems={orderedHistories}
-              activeHistory={activeHistory}
-              onSelect={(item) => setActiveHistory(item)}
-            />
-            <HistoryTree
-              activeNodeId={3}
-              data={[
-                {
-                  key: 1,
-                  description: "2020-12-20",
-                  nodes: [
-                    {
-                      id: 1,
-                      label: "NFT created",
-                    },
-                  ],
-                },
-                {
-                  key: 2,
-                  description: "2020-12-22",
-                  nodes: [
-                    {
-                      id: 2,
-                      label: "NFT re-created",
-                    },
-                    {
-                      id: 3,
-                      label: "Documents added",
-                    },
-                  ],
-                },
-              ]}
-            />
-          </HistoryTreeContainer>
-        </MainContentContainer>
-      </>
+        </Flex>
+
+        <Flex item xs={12} md={4} pr={4}>
+          <HistoryPicker
+            historyItems={orderedHistories}
+            activeHistory={activeHistory}
+            onSelect={(item) => setActiveHistory(item)}
+          />
+          <HistoryTree
+            activeNodeId={3}
+            data={[
+              {
+                key: 1,
+                description: "2020-12-20",
+                nodes: [
+                  {
+                    id: 1,
+                    label: "NFT created",
+                  },
+                ],
+              },
+              {
+                key: 2,
+                description: "2020-12-22",
+                nodes: [
+                  {
+                    id: 2,
+                    label: "NFT re-created",
+                  },
+                  {
+                    id: 3,
+                    label: "Documents added",
+                  },
+                ],
+              },
+            ]}
+          />
+        </Flex>
+      </Flex>
     );
   }
 );
