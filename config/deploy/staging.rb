1bd708ff3debc6c356e2d2e82202f0560f13c8c1
Majid Amiri <majidamiriii@gmail.com>
2021-02-09 12:02:19 +0330

    fix file namings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :application_name, "metaproof_preview"
 set :application_port, 4001
 set :instance, "preview"
-set :deploy_branch, "preview"
+set :deploy_branch, "develop"
 
 server "138.197.183.107", user: "deployer", roles: %w{app}, port: 28345
