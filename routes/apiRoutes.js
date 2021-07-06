7ce74cd3574f724e60f1cca4fca80a48180a045d
Majid Amiri <majidamiriii@gmail.com>
2021-07-06 13:08:49 +0430

    add ability to fetch data from different apis

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,10 @@
 import { getBaseUrl } from "utils/url";
 
-const { NEXT_PUBLIC_CREATORSHUB_URL } = process.env;
-
 export const apiRoutes = {
   creatorshub: {
-    getStats: () => `${NEXT_PUBLIC_CREATORSHUB_URL}/api/public/stats`,
-    getNfts: (q) =>
-      `${NEXT_PUBLIC_CREATORSHUB_URL}/api/public/nfts${q ? `?q=${q}` : ""}`,
-    getCreators: () => `${NEXT_PUBLIC_CREATORSHUB_URL}/api/public/creators`,
+    getStats: () => `/api/public/stats`,
+    getNfts: (q) => `/api/public/nfts${q ? `?q=${q}` : ""}`,
+    getCreators: () => `/api/public/creators`,
     getNftPdf: (nftId, createdWith, locale = "en") =>
       `${getBaseUrl(
         createdWith
